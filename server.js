db0fcac65bcabc512bb8559a70d7516db1faaa7e
Abhishek Chauhan <chauhan.abhishek335@gmail.com>
2026-07-20 23:50:19 +0530

    Revert "chore: update UI color palette and fetch latest road updates for dashboard display"
    This reverts commit b748539b7d4770d586a5305d3241b0ad0222f239.

--- server.js
+++ server.js
@@ -235,16 +235,9 @@ app.get('/dashboard', requireAuth, async (req, res) => {
         include: [{ model: Ride, as: 'ride' }]
     });
 
-    const roadUpdates = await RoadUpdate.findAll({
-        include: [{ model: User, as: 'user', attributes: ['id', 'name'] }],
-        order: [['timestamp', 'DESC']],
-        limit: 5
-    });
-
     res.render('dashboard.html', {
         active_rides: active_rides,
-        requests_made: requests_made,
-        roadUpdates: roadUpdates
+        requests_made: requests_made
     });
 });
 
