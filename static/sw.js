fd5b858a5fb9355d8e75fd5366755fbc5eb89aac
Abhishek Chauhan <chauhan.abhishek335@gmail.com>
2026-07-24 08:30:36 +0530

    feat: implement service worker caching and PWA manifest support

--- static/sw.js
+++ static/sw.js
@@ -22,3 +22,75 @@ self.addEventListener('notificationclick', function(event) {
         );
     }
 });
+
+const CACHE_NAME = 'karmaride-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  '/static/img/icon-192x192.png',
+  '/static/img/icon-512x512.png',
+  '/static/img/hero_bg.png',
+  '/static/img/how_it_works.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Only handle GET requests
+  if (event.request.method !== 'GET') {
+      return;
+  }
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                // Ignore non-http/https requests
+                if (event.request.url.startsWith('http')) {
+                    cache.put(event.request, responseToCache);
+                }
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
